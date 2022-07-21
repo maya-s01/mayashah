7f84f119c6cc98838279cd7ba40da8c3dab619fa
John Oerter <john@johnoerter.me>
2022-07-21 16:35:10 -0400

    Use chalk!

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 #!/usr/bin/env node
 
+import chalk from 'chalk';
+
 console.log(`
 Hi, nice to meet you!
 
-I am passionate about building beautiful web applications that delight and inspire.
+I am passionate about building beautiful web applications that ${chalk.underline.blue(
+  'delight'
+)} and ${chalk.underline.blue('inspire')}.
 
-I have over 8 years of experience working in a variety of industries building solutions to problems using software. I prefer to focus on the UI and work closely with end users and designers to craft elegant experiences. I believe that striving for simplicity and clarity results in the best outcomes.
+I have over 8 years of experience working in a variety of industries building solutions to problems 
+using software. I prefer to focus on the UI and work closely with end users and designers to craft 
+elegant experiences. 
+I believe that striving for ${chalk.underline.blue(
+  'simplicity'
+)} and ${chalk.underline.blue('clarity')} results in the best outcomes.
 
 If you would like to read some of my thoughts on building software, checkout my blog at redgreenrefactor.dev.
 
