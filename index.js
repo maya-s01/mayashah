b101b1384aefd80765c67d160bed0f4f1e4d3370
John Oerter <john@johnoerter.me>
2022-07-21 15:10:27 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log(`
+Hi, nice to meet you!
+
+I am passionate about building beautiful web applications that delight and inspire.
+
+I have over 8 years of experience working in a variety of industries building solutions to problems using software. I prefer to focus on the UI and work closely with end users and designers to craft elegant experiences. I believe that striving for simplicity and clarity results in the best outcomes.
+
+If you would like to read some of my thoughts on building software, checkout my blog at redgreenrefactor.dev.
+
+To get in contact with me, send me an email at hey@johnoerter.me, follow me on GitHub, connect with me on LinkedIn, or join me in the #fediverse on mastodon.technology!
+`);
